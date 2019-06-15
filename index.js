a45837b0ddb89300dc641929404a4e0057c617de
Nath38 <49879076+Nath38@users.noreply.github.com>
2019-06-15 12:51:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const fs = require('fs')
 const client = new Discord.Client()
 
-var prefix = ('p.')
+var prefix = ('!')
 
 client.on('ready', () => {
   console.log('Le bot est prêt!')
