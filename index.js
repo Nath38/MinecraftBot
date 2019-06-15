37413a9d26b5e1e0e2332ebac6c887a86230bb2d
Nath38 <49879076+Nath38@users.noreply.github.com>
2019-06-15 14:52:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,20 @@ client.on('message', async message => {
   }
 });
 
+client.on("message", message => {
+  if (!message.guild) return
+  let args = message.content.trim().split(/ +/g)
+
+  if (args[0].toLowerCase() === prefix + "clear") {
+      if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+      let count = args[1]
+      if (!count) return message.channel.send("Veuillez indiquer un nombre de messages à supprimer")
+      if (isNaN(count)) return message.channel.send("Veuillez indiquer un nombre valide")
+      if (count < 1 || count > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
+      message.channel.bulkDelete(parseInt(count) + 1)
+  }
+})
+
 client.on('message', async message => {
   if(message.content.startsWith(prefix + 'say')){
 
