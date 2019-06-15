365b0df3420674f808bf6b652be96f1db0c70e2b
Nath38 <49879076+Nath38@users.noreply.github.com>
2019-06-15 12:35:19 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+const fs = require('fs')
+const client = new Discord.Client()
+
+var prefix = ('p.')
+
+client.on('ready', () => {
+  console.log('Le bot est prêt!')
+});
+
+
+
+
