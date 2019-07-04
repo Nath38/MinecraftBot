514de13a49d3391b9086e9a2c5b723bb04b4431b
Nath38 <49879076+Nath38@users.noreply.github.com>
2019-07-04 18:25:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,41 @@ client.on("message", message => {
       if (count < 1 || count > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
       message.channel.bulkDelete(parseInt(count) + 1)
   }
+  
+  if (args[0].toLowerCase() === prefix + "mute") {
+      if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+      let member = message.mentions.members.first()
+      if (!member) return message.channel.send("Membre introuvable")
+      if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas mute ce membre")
+      if (member.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition || member.id === message.guild.ownerID) return message.channel.send("Je ne peux pas mute ce membre")
+      let muterole = message.guild.roles.find(role => role.name === 'mute')
+      let userrole = message.guild.roles.find(role => role.name === 'joueur')
+      if (muterole) {
+          member.removeRole(userrole)
+          member.addRole(muterole)
+          message.channel.send(member + ' a été mute :white_check_mark:')
+      }
+      else {
+          message.guild.createRole({name: 'Muted', permissions: 0}).then((role) => {
+              message.guild.channels.filter(channel => channel.type === 'text').forEach(channel => {
+                  channel.overwritePermissions(role, {
+                      SEND_MESSAGES: false
+                  })
+              })
+              member.addRole(role)
+              message.channel.send(member + ' a été mute :white_check_mark:')
+          })
+      }
+  }
 })
+
+client.on('message', message => {
+    if (message.content === prefix + 'rip') {
+      message.channel.send('https://i.imgur.com/w3duR07.png')
+      message.delete()
+    }
+  })
+
 
 client.on('message', message => {
   if (message.content === prefix + 'membre') {
